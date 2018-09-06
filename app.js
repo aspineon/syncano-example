15065d37a412bdf662431160bd5c32709f1190b0
Maciej Korsan <maciejkorsan@gmail.com>
2018-09-06 12:04:24 +0200

    feat: initial version

---
+++ app.js
@@ -0,0 +1,23 @@
+//initialising syncano client
+var s = new SyncanoClient('silent-flower-7664');
+
+var button = document.querySelector('.submit--js');
+
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+  var firstName = document.querySelector('.firstname--js').value;
+  var lastName = document.querySelector('.lastname--js').value;
+  if (firstName.length > 0 && lastName.length > 0) {
+    document.querySelector('body').classList.add('loading')
+    // calling syncano socket!
+    s.get('hello/hello', {
+      firstname: firstName,
+      lastname: lastName
+    }).then(function(result) {
+      document.querySelector('body').classList.add('completed')
+      document.querySelector('.response__content--js').innerHTML = result.message;
+    });
+  } else {
+    alert('Fill the fields before calling the socket!')
+  }
+});
